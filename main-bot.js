759e648c9ad58dfe372f05f38f414b8ac247493e
azkazunami36 <kazunami36.sum@icloud.com>
2022-09-17 12:48:56 +0900

    コードの小修整

--- main-bot.js
+++ main-bot.js
@@ -1,7 +1,7 @@
 const { Client, GatewayIntentBits, Partials, EmbedBuilder, BaseChannel, ApplicationCommandOptionType, ChannelType } = require('discord.js'); //Discord.js本体
 const { entersState, createAudioPlayer, createAudioResource, joinVoiceChannel, getVoiceConnections, VoiceConnection, StreamType } = require('@discordjs/voice'); //Discord.jsVoice
 const ytdl = require('ytdl-core'); //YouTube取得用
-var config = { prefix: "!plays", channelid: "1013747732588023818" }; //json
+var config = { prefix: "!plays" }; //json
 var token = require("./token.json"); //トークン
 const client = new Client({
   partials: [Partials.Channel],
@@ -21,25 +21,28 @@ client.on("ready", () => {
 });
 
 var connection; //接続や切断を管理
+var stream;
 
 client.on("messageCreate", async message => {
   if (message.author.bot) return; //チャット送信者がbotならリターン(終了)
-  
+
   if (message.content.startsWith(config.prefix)) {
     var connd = message.content.split(" "); //これはテキストにスペースがあれば、そのスペースを挟んでる文字列を取り出すコード(説明がごみ)
+    var channel = message.member.voice.channel;
+    console.log(channel);
     console.log(connd);
     if (connd[1] == "play") {
+      if (!channel) return message.reply(message.author.username + "さんがボイスチャットにいません...\n入ってからまたやってみてくださいね！");
       var url = connd[2]; //URL設定
       connection = joinVoiceChannel({ //うまく説明はできないけど、ボイスチャンネル参加
         adapterCreator: message.guild.voiceAdapterCreator, //わからん
-        channelId: config.channelid, //VoiceChannelを設定
+        channelId: channel.id, //VoiceChannelを設定
         guildId: message.guildId, //サーバーIDを設定
         selfDeaf: true //多分スピーカーミュート
       });
-      console.log(getVoiceConnections());
       var player = createAudioPlayer(); //多分音声を再生するためのもの
       connection.subscribe(player); //connectionにplayerを登録？
-      var stream = ytdl(ytdl.getURLVideoID(url), {
+      stream = ytdl(ytdl.getURLVideoID(url), {
         filter: format => format.audioCodec === 'opus' && format.container === 'webm', //webm opus //わからん
         quality: 'highest', //わからん
         highWaterMark: 32 * 1024 * 1024, //わからん
@@ -48,10 +51,10 @@ client.on("messageCreate", async message => {
       // 再生
       player.play(resource); //再生
     } else if (connd[1] == "stop") {
-      connection.destroy();
+      stream.destroy(); //わからんけど自分が付け足した切断
+      connection.destroy(); //VCの切断
     };
   };
 });
-
 
 client.login(token); //ログイン
