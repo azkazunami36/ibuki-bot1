521050957840e032a64796a2c7f5a276bb1ab54c
azkazunami36 <kazunami36.sum@icloud.com>
2022-09-17 12:56:47 +0900

    コードの修正

--- main-bot.js
+++ main-bot.js
@@ -1,7 +1,7 @@
 const { Client, GatewayIntentBits, Partials, EmbedBuilder, BaseChannel, ApplicationCommandOptionType, ChannelType } = require('discord.js'); //Discord.js本体
 const { entersState, createAudioPlayer, createAudioResource, joinVoiceChannel, getVoiceConnections, VoiceConnection, StreamType } = require('@discordjs/voice'); //Discord.jsVoice
 const ytdl = require('ytdl-core'); //YouTube取得用
-var config = { prefix: "!plays" }; //json
+var config = { prefix: "!" }; //json
 var token = require("./token.json"); //トークン
 const client = new Client({
   partials: [Partials.Channel],
@@ -21,7 +21,7 @@ client.on("ready", () => {
 });
 
 var connection; //接続や切断を管理
-var stream;
+var stream; //ストリーム？
 
 client.on("messageCreate", async message => {
   if (message.author.bot) return; //チャット送信者がbotならリターン(終了)
@@ -32,8 +32,9 @@ client.on("messageCreate", async message => {
     console.log(channel);
     console.log(connd);
     if (connd[1] == "play") {
-      if (!channel) return message.reply(message.author.username + "さんがボイスチャットにいません...\n入ってからまたやってみてくださいね！");
       var url = connd[2]; //URL設定
+      if (!ytdl.validateURL(url)) return message.reply("`" + url + "`が理解できませんでした..."); //ytdlがURL解析してくれるらしい
+      if (!channel) return message.reply(message.author.username + "さんがボイスチャットにいません...\n入ってからまたやってみてくださいね！"); //ボイスチャンネルにいるかの存在確認
       connection = joinVoiceChannel({ //うまく説明はできないけど、ボイスチャンネル参加
         adapterCreator: message.guild.voiceAdapterCreator, //わからん
         channelId: channel.id, //VoiceChannelを設定
@@ -42,8 +43,8 @@ client.on("messageCreate", async message => {
       });
       var player = createAudioPlayer(); //多分音声を再生するためのもの
       connection.subscribe(player); //connectionにplayerを登録？
-      stream = ytdl(ytdl.getURLVideoID(url), {
-        filter: format => format.audioCodec === 'opus' && format.container === 'webm', //webm opus //わからん
+      stream = ytdl(ytdl.getURLVideoID(url), { //ストリームを使うらしいけど、意味わからない
+        filter: format => format.audioCodec === 'opus' && format.container === 'webm', //webm？ opus？ しらね(いやwebmとかopusとかは知ってる)
         quality: 'highest', //わからん
         highWaterMark: 32 * 1024 * 1024, //わからん
       });
