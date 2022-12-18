d3a40429afca3ede070cd43365a897eb88f88ee3
AzkSumWave36 <kazunami36.sum@icloud.com>
2022-12-19 00:42:39 +0900

    コード小修整

--- main-bot.js
+++ main-bot.js
@@ -162,7 +162,6 @@ client.on(Events.MessageCreate, async message => {
         break;
       }
       case "repeat": {
-
         switch (subcontent) {
           case "repeat": channeldata.repeat = 1; break;
           case "one-repeat": channeldata.repeat = 2; break;
@@ -204,7 +203,7 @@ client.on(Events.MessageCreate, async message => {
         break;
       }
     };
-    savejson(clientdata);
+    savejson(clientdata, "music_botv2");
   };
 });
 const ytplay = async (guildid, voiceid) => {
@@ -219,7 +218,7 @@ const ytplay = async (guildid, voiceid) => {
         channeldata.playing += 1;
         if (plist.length == channeldata.playing || !channeldata.playing) channeldata.playing = 0;
       };
-      savejson(clientdata);
+      savejson(clientdata, "music_botv2");
       if (clientdata.cacheis) {
         if (!fs.existsSync("ytaudio/" + plist[channeldata.playing].url + ".mp3")) continue;
         server.ytstream = "ytaudio/" + plist[channeldata.playing].url + ".mp3"; //ファイルパスを取得
@@ -300,13 +299,13 @@ const videoembed = async (content, data) => {
 * @returns - 時間、分、秒が組み合わさった文字列を出力します。
 */
 const timeString = async seconds => {
-   let minutes = 0, hour = 0, timeset = "";
-   for (minutes; seconds > 59; minutes++) seconds -= 60;
-   for (hour; minutes > 59; hour++) minutes -= 60;
-   if (hour != 0) timeset += hour + "時間";
-   if (minutes != 0) timeset += minutes + "分";
-   if (seconds != 0) timeset += seconds + "秒";
-   return timeset;
+  let minutes = 0, hour = 0, timeset = "";
+  for (minutes; seconds > 59; minutes++) seconds -= 60;
+  for (hour; minutes > 59; hour++) minutes -= 60;
+  if (hour != 0) timeset += hour + "時間";
+  if (minutes != 0) timeset += minutes + "分";
+  if (seconds != 0) timeset += seconds + "秒";
+  return timeset;
 };
 /**
 * jsonデータを入力をもとにコピーを作成する
@@ -318,37 +317,37 @@ const jsonRebuild = j => { return JSON.parse(JSON.stringify(j)); };
 * data-serverからmusic_botのjsonデータを取得する
 */
 const jsonload = () => {
-   return new Promise((resolve, reject) => {
-       const req = require("http").request("http://localhost", {
-           port: 3000,
-           method: "post",
-           headers: { "Content-Type": "text/plain;charset=utf-8" }
-       });
-       req.on("response", res => {
-           let data = "";
-           res.on("data", chunk => { data += chunk; });
-           res.on("end", () => { resolve(data); });
-       });
-       req.write(JSON.stringify(["music_botv2"]));
-       req.on("error", reject);
-       req.end();
-   });
+  return new Promise((resolve, reject) => {
+    const req = require("http").request("http://localhost", {
+      port: 3000,
+      method: "post",
+      headers: { "Content-Type": "text/plain;charset=utf-8" }
+    });
+    req.on("response", res => {
+      let data = "";
+      res.on("data", chunk => { data += chunk; });
+      res.on("end", () => { resolve(data); });
+    });
+    req.write(JSON.stringify(["music_botv2"]));
+    req.on("error", reject);
+    req.end();
+  });
 };
 /**
 * data-serverにmusic_botのjsonデータを格納する
-* @param {*} j 
+* @param {*} j 格納するjsonを入力
 */
-const savejson = async j => {
-   return new Promise((resolve, reject) => {
-       const req = require("http").request("http://localhost", {
-           port: 3000,
-           method: "post",
-           headers: { "Content-Type": "text/plain;charset=utf-8" }
-       });
-       req.on("response", resolve);
-       req.write(JSON.stringify(["music_botv2", j]));
-       req.on("error", reject);
-       req.end();
-   });
+const savejson = async (j, name) => {
+  return new Promise((resolve, reject) => {
+    const req = require("http").request("http://localhost", {
+      port: 3000,
+      method: "post",
+      headers: { "Content-Type": "text/plain;charset=utf-8" }
+    });
+    req.on("response", resolve);
+    req.write(JSON.stringify([name, j]));
+    req.on("error", reject);
+    req.end();
+  });
 };
-client.login(process.env.token); //ログイン
+client.login(process.env.token);
